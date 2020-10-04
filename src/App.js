1c27470c2d02ce4a1ab8abdf5f8fb95e43bbfb61
KRAILERKM <krailerk_man@hotmail.com>
2020-10-04 15:44:41 +0700

    4-10-2020 learning

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
+      <Header title = "Test Dev" />
+
       <h1 className="hello-react"><i>Hello World!!</i></h1>
+      <table border="1">
+        <tr>
+          <td>Number</td>
+          <td>Description</td>
+          <td>999</td>
+        </tr>
+        <tr>
+          <td>001</td>
+          <td>My name is Hello!</td>
+          <td>888</td>
+        </tr>
+        <tr>
+          <td>002</td>
+          <td>Hi!</td>
+          <td>777</td>
+        </tr>
+      </table>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
