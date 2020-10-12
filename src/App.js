aeed6a6a761e4a04d9908c542453b8ba46fd1509
KRAILERKM <krailerk_man@hotmail.com>
2020-10-12 21:54:01 +0700

    10-12-2020-2153 Learn React

--- src/App.js
+++ src/App.js
@@ -4,43 +4,49 @@ import './App.css';
 import Header from './Header';
 
 function App() {
+  const fullname = "Krailerk M.";
+  const num1 = 111;
+  const num2 = 222;
   return (
-    <div className="App">
-      <Header title = "Test Dev" />
-
-      <h1 className="hello-react"><i>Hello World!!</i></h1>
-      <table border="1">
-        <tr>
-          <td>Number</td>
-          <td>Description</td>
-          <td>999</td>
-        </tr>
-        <tr>
-          <td>001</td>
-          <td>My name is Hello!</td>
-          <td>888</td>
-        </tr>
-        <tr>
-          <td>002</td>
-          <td>Hi!</td>
-          <td>777</td>
-        </tr>
-      </table>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header title = {fullname} />
+      <p>Sum : {num1 / num2}</p>
+        <div className="App">
+          <h1 className="hello-react"><i>Hello World!!</i></h1>
+          <table border="1">
+            <tr>
+              <td>Number</td>
+              <td>Description</td>
+              <td>999</td>
+            </tr>
+            <tr>
+              <td>001</td>
+              <td>My name is Hello!</td>
+              <td>888</td>
+            </tr>
+            <tr>
+              <td>002</td>
+              <td>Hi!</td>
+              <td>777</td>
+            </tr>
+          </table>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+      </div>
     </div>
+
   );
 }
 
