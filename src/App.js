057b824c363a106fe455aceebf177c52ba4067e8
KRAILERKM <krailerk_man@hotmail.com>
2020-11-01 22:34:37 +0700

    11-1-2020-1034 Learning

--- src/App.js
+++ src/App.js
@@ -10,9 +10,37 @@ function App() {
   return (
     <div>
       <div class="MenuBarTextAlign">
+        <div class="brn-toolbar" role="toolbar">
+          <div class="btn-group mr-3" role="group" aria-label="Group 1">
+            <button type="button" class="btn btn-primary">First</button>
+            <button type="button" class="btn btn-primary">Second</button>
+            <button type="button" class="btn btn-primary">Third</button>
+          </div>
+          <div class="btn-group" role="group" aria-label="Group 2">
+            <button type="button" class="btn btn-primary">First</button>
+            <button type="button" class="btn btn-primary">Second</button>
+          </div>
+        </div><br />
+        <div class="btn-group">
+          <button class="btn">Index</button>
+          <button class="btn">Webboard</button>
+          <button class="btn">Story</button>
+          <button class="btn">Contract</button>
+        </div><hr />
         <a target="_blank" rel="noopener" class="alert alert-danger" href="#">First Page</a>
         <a target="_blank" rel="noopener" class="alert alert-info" href="#">Store</a>
         <a target="_blank" rel="noopener" class="alert alert-success" href="#">Contract</a>
+        <button type="button">Normal</button>
+        <button type="button" class="btn">btn</button>
+        <button type="button" class="btn btn-primary">btn btn-primary</button><hr />
+        <button type="button" class="btn btn-secondary">btn btn-secondary</button>
+        <button type="button" class="btn btn-info">btn btn-info</button>
+        <button type="button" class="btn btn-waring">btn btn-waring</button>
+        <button type="button" class="btn btn-danger">btn btn-danger</button>
+        <button type="button" class="btn btn-success">btn btn-success</button><hr />
+        <button type="button" class="btn btn-dark">btn btn-dark</button>
+        <button type="button" class="btn btn-light">btn btn-light</button>
+        <button type="button" class="btn btn-link">btn btn-link</button>
       </div>
       <Header title = {fullname} />
       <Header title = "Hello world"/>
