b3cae7e82ff3379ed007ea7591b8ca022d7ab9a2
KRAILERKM <krailerk_man@hotmail.com>
2020-11-13 06:52:48 +0700

    11-13-2020-0652

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 function App() {
   const iniData = 0;
